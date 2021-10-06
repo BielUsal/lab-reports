e38c2af09a372573c3717f3eb27732e5f719e8e6
Biel <gabdandrade@usal.es>
2021-10-06 14:14:46 +0200

    almost done with introduction

--- 2/Mec/5/grapher3.m
+++ 2/Mec/5/grapher3.m
@@ -1,11 +1,11 @@
 A = importdata('mt.csv');
 m = A(:,1);
 mer =10^(-2)* A(:,2);
-R = 0.15;
+R = 0.260;
 b = A(:,3);
 T = (R*((9.81*b.^2)/(4*pi^2)-R)).^(-1);
 Ter = A(:,4);
-x=linspace(0.6,2.6,1000);
+x=linspace(0,1.7,1000);
 c = polyfit(T,m,1);
 hold on
 errorbar(T,m,mer,mer,Ter,Ter, '.')
