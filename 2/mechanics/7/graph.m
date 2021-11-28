5ee00ddf9ab845fef5adb70891fcfdcfee0958de
biel <gabdandrade@usal.es>
2021-11-28 17:12:25 +0100

    lelectro

--- 2/mechanics/7/graph.m
+++ 2/mechanics/7/graph.m
@@ -1,15 +1,15 @@
 
 x = A(:,1);
-xer = 0.1*10^(-2);
+xer = ones(9,1) * 0.1;
 T = A(:,2);
-Ter = 0.1;
+Ter = ones(9,1) * 0.1*10^(-2);
 a=linspace(0.05,1.8,1000);
 c = polyfit(T,x,1);
 hold on
 box on
 grid on
 errorbar(T,x,xer,xer,Ter,Ter, '.')
-xlabel('{\lambda}\textit (m)')
+xlabel(' \lambda (m)')
 ylabel('f^{-1}')
 plot(a,polyval(c,a))
 xlix([0.05, 1.8])
